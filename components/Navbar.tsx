829a0a4991cf9dcab5eedd4f32abbab1692a7d55
sunandasamanta <samantasunanda724@gmail.com>
2024-05-23 22:43:39 +0530

    init and navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,9 @@
+import PublicIcon from '@mui/icons-material/Public';
+
+export default function Navbar() {
+  return (
+    <nav className="h-16 bg-[#F55A5A] gap-1 w-full text-white flex justify-center flex-wrap content-center text-lg">
+        <PublicIcon /> airbnb.com by @bakshiCodes
+    </nav>
+  )
+}
